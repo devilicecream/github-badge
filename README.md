c4b99bb9d3890980ce35859438ac5059bf00564c
Walter Danilo Galante <walterdangalante@gmail.com>
2015-02-12 19:17:19 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Github Badge
+Very simple *Github* badge embeddable in every website.
+It uses the public *Github* APIs to get info about the specified user and his contibution.
+
+## Included
+Easily customizable via CSS, it comes with Boostrap 3 support (you will need to write less CSS to make it look fancy)
+and two CSS files that make it look good without any effort.
+
+## Requirements
+The only requirement is jQuery.
+
+## How does it work
+Include the CSS file and jQuery at the top of your HTML page, and JS file at the bottom.
+
+```html
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" media="screen" href="<path to>/github-badge.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>    
+    </head>
+    <body>
+        <script type="text/javascript" src="<path to>/github-badge.js"></script>
+    </body>
+</html>
+
+```
+
+Add a div element with id `github-badge` (You can choose your own element id, but then you'll have to edit the default CSS file)
+
+```html
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" media="screen" href="<path to>/github-badge.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>    
+    </head>
+    <body>
+        
+        <div id="github-badge"></div>
+        
+        <script type="text/javascript" src="<path to>/github-badge.js"></script>
+    </body>
+</html>
+
+```
+
+Initialize the badge with this JS string
+
+```javascript
+new GitHubBadge(<'#github-badge' or a jQuery selector that picks the badge element>, <github username>);
+```
+
+## Examples
+There are a couple of examples in the *example* folder.
+The first one `no_bootstrap.html`, as you can tell, doesn't use bootstrap to render the badge, while the second one `with_bootstrap.html` does.
+
+
+### Enjoy!
---
+++ github-badge.js
@@ -0,0 +1,80 @@
+function GitHubBadge(selector, username, options) {
+    this.$element = $(selector);
+    this.username = username;
+    this.options = options || {};
+    this.buildBadge();
+}
+
+GitHubBadge.prototype = {
+    buildHTML: function () {
+        var side_class = this.options.side_class || 'col-xs-4';
+        var main_class = this.options.main_class || 'col-xs-8';
+        var $side = $("<div id='github-badge-side' class='" + side_class + "'></div>");
+        var $main = $("<div id='github-badge-main' class='" + main_class + "'></div>");
+
+        var avatar_class = this.options.avatar_class || 'img-thumbnail';
+        var avatar = $("<img src='" + this.user.avatar_url + "' class='" + avatar_class + "' id='github-badge-avatar' />");
+        var name = $("<h2 id='github-badge-name'>" + this.user.name + "</h2>");
+        var login = $("<h4 id='github-badge-login'>" + this.user.login + "</h4>");
+        var $profile_link = $("<a href='" + this.user.url + "' target='_blank'></a>");
+        $profile_link.append(avatar);
+        $profile_link.append(name);
+        $profile_link.append(login);
+
+        var followers_class = this.options.followers_class || 'col-xs-6';
+        var followers = $("<div class='" + followers_class + "' id='github-badge-followers'><h3>" + this.user.followers + "</h3>Followers</div>");
+        var following_class = this.options.following_class || 'col-xs-6';
+        var following = $("<div class='" + following_class + "' id='github-badge-following'><h3>" + this.user.following + "</h3>Following</div>");
+        $side.append($profile_link);
+        $side.append(followers);
+        $side.append(following);
+
+        var title = $("<h3>Contributions</h3>");
+        var $list = $("<ul id='github-badge-ul'></ul>");
+        this.repositories.forEach(function (repo) {
+            var repository = $("<li class='github-badge-li'><a target='_blank' href='" + repo.html_url + "'>" + repo.name + "</a></li>");
+            $list.append(repository);
+        });
+        var last_push_date = this.repositories[0].pushed_at;
+        var last_push = $("<p id='github-badge-lastpush'>Last push: <span>" + new Date(last_push_date).toLocaleString() + "</span></p>");
+
+        $main.append(title);
+        $main.append($list);
+        $main.append(last_push);
+
+        var github_logo_url = 'https://assets-cdn.github.com/favicon.ico'
+        var github_logo = $("<a href='http://github.com' target='_blank' id='github-badge-logo'><img src='" + github_logo_url + "' id='github-badge-logo' style='height: 16px; width: 16px' /></a>");
+
+        this.$element.append(github_logo);
+        this.$element.append($side);
+        this.$element.append($main);
+
+        var tail = this.options.tail || $('<div class="clearfix"></div>');
+        if (tail) {
+            this.$element.append(tail);
+        }
+    },
+    getUser: function () {
+        var _this = this;
+        $.ajax({url: 'https://api.github.com/users/' + this.username,
+                success: function (data) { _this.gotUser(data)}});
+    },
+    gotUser: function (data) {
+        this.user = data;
+        this.getUserRepositories();
+    },
+    getUserRepositories: function () {
+        var _this = this;
+        $.ajax({url: 'https://api.github.com/users/' + this.username + '/repos',
+                data: {type: 'all', sort: 'pushed'},
+                success: function (data) { _this.gotUserRepositories(data)}});
+    },
+    gotUserRepositories: function (data) {
+        this.repositories = data;
+        this.buildHTML();
+    },
+    buildBadge: function () {
+        this.getUser();
+    }
+}
+
